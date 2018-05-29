11485636621591ce08eb672e293eb1cd28a0346d
hanxiaoqing <han@github.com>
2018-05-29 15:41:33 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,66 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import LoginComponent from './LoginComponent'
 import {
-  Form,
-  Icon,
-  Button,
-  Input,
-  Checkbox
-} from 'antd';
-
-const FromItem = Form.Item;
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 class App extends Component {
+  islogin = false
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log("values of form", values);
-      }
-    });
-  }
-
-
-  fieldDecorator(type, nilErrMsg) {
-    const { getFieldDecorator } = this.props.form;
-    return getFieldDecorator(type, { rules: [{ required: true, message: nilErrMsg }] });
-  }
-
-
+   roster= () => (
+    <Switch>
+          <Route path='/register' component={LoginComponent}/>
+    </Switch>
+  );
 
 
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const showwhat = this.islogin ? <div>已经登录</div> : <LoginComponent />
     return (
-      <div className="form-contaner">
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <FromItem className="form-item">
-            {this.fieldDecorator('userName', 'please input your username!')(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0, .25)' }} />} placeholder="Username" />
-            )}
-          </FromItem>
-          <FromItem className="form-item">
-            {this.fieldDecorator('password', 'please input your Password!')(
-              <Input type="password" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0, .25)' }} />} placeholder="Password" />
-            )}
-          </FromItem>
-          <FromItem className="form-item">
-            {getFieldDecorator('remember', {
-              valueProoName: 'checked',
-              initialValue: true
-            })(
-              <Checkbox>记住密码</Checkbox>
-            )}
-            <a className="login-form-forgot" href="">忘记密码</a>
-            <Button type="primary" htmlType="submit" className="login-form-button"> 登陆</Button>
-            <a href="">账号注册</a>
-          </FromItem>
-        </Form>
-      </div>
-
+      <div>
+        {this.roster()}
+        <Link to='/register'>register</Link>
+        </div>
     );
   }
 }
 
-export default Form.create()(App);
+export default App;
---
+++ src/LoginComponent.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import {
+  Form,
+  Icon,
+  Button,
+  Input,
+  Checkbox,
+  Select,
+} from 'antd';
+
+const FromItem = Form.Item;
+
+const userNamePattern = /^[a-zA-Z0-9]{6,14}$/;
+const passwordPattern = /^[a-zA-Z0-9]{6,14}$/;
+
+
+
+class LoginComponent extends Component {
+  handleSubmit = (e) => {
+      e.preventDefault();
+      this.props.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("values of form", values);
+        }
+      });
+    }
+  
+  fieldDecorator(type, nilErrMsg, pattern) {
+    const { getFieldDecorator } = this.props.form;
+    const rule = { 
+      required: true,
+      message: nilErrMsg,
+      pattern: pattern,
+    }
+    return getFieldDecorator(type, {rules: [rule]});
+  }
+  
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="form-contaner">
+        <div className="form-header-label">欢迎登陆</div>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <FromItem className="form-item">
+            {this.fieldDecorator('userName', '请输入6~14位字母或数字', userNamePattern)(
+              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0, .25)' }} />} placeholder="用户名" />
+            )}
+          </FromItem>
+
+          <FromItem className="form-item">
+            {this.fieldDecorator('password', '请输入6~14位字母或数字', passwordPattern)(
+              <Input type="password" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0, .25)' }} />} placeholder="密码" />
+            )}
+          </FromItem>
+
+          <FromItem>
+            {this.fieldDecorator('loginStanding', '请选择登陆身份')(
+              <Select
+                placeholder="选择你的登陆身份"
+                onchange={this.handleSelectChange}              
+              >
+                <Select.Option value="writer">w</Select.Option>
+                <Select.Option value="serviceAgent">s</Select.Option>
+                <Select.Option value="management">m</Select.Option>
+              </Select>
+            )}
+          </FromItem>
+
+          <FromItem className="form-item">
+            {getFieldDecorator('remember', {
+              valueProoName: 'checked',
+              initialValue: false
+            })(
+              <Checkbox>记住密码</Checkbox>
+            )}
+
+            <a className="login-form-forgot" href="">忘记密码</a>
+            <Button type="primary" htmlType="submit" className="login-form-button"> 登陆</Button>
+            <a href="">账号注册</a>
+          </FromItem>
+        </Form>
+      </div>
+
+    );
+  }
+}
+
+
+export default Form.create()(LoginComponent);
+
